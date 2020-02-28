94dc7759b713d365381fb1596a9b5e97b07d1c83
Krystof <krystofmacek333@gmail.com>
2020-02-28 18:50:04 +0100

    Initial commit - connected to Firebase services

--- app/src/main/java/com/krystofmacek/firebasechatapp/activity/SplashScreenActivity.java
+++ app/src/main/java/com/krystofmacek/firebasechatapp/activity/SplashScreenActivity.java
@@ -2,15 +2,42 @@ package com.krystofmacek.firebasechatapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.krystofmacek.firebasechatapp.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private FirebaseUser user;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+
+        Intent intent;
+
+        if(user == null) {
+            intent = new Intent(getApplicationContext(), SignupActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+
+        } else {
+            intent = new Intent(getApplicationContext(), MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
+
+
     }
 }
