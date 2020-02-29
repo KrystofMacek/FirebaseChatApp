f597a697e3ab1892a2219d991832ff6c21018daa
Krystof <krystofmacek333@gmail.com>
2020-02-29 15:30:01 +0100

    Top toolbar and bottom navigation

--- app/src/main/java/com/krystofmacek/firebasechatapp/activity/MainActivity.java
+++ app/src/main/java/com/krystofmacek/firebasechatapp/activity/MainActivity.java
@@ -1,16 +1,80 @@
 package com.krystofmacek.firebasechatapp.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.graphics.BlendMode;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 import com.krystofmacek.firebasechatapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView heading;
+    BottomNavigationView botNavigation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        setupNavigation();
+
+    }
+
+    private void setupNavigation() {
+
+        heading = findViewById(R.id.toolbar_heading);
+        heading.setText("Home");
+
+        Toolbar toolbar = findViewById(R.id.toolbar_top);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+
+        botNavigation = findViewById(R.id.bot_navigation);
+        botNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    //TODO: For each item ID navigate
+                    case R.id.nav_item_home:
+                        break;
+                    case R.id.nav_item_friends:
+                        break;
+                    case R.id.nav_item_search:
+                        break;
+                    case R.id.nav_item_chats:
+                        break;
+                }
+                return false;
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_bar_dropdown_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout:
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(MainActivity.this, SignupActivity.class));
+                finish();
+                return true;
+        }
+        return false;
     }
 }
--- app/src/main/java/com/krystofmacek/firebasechatapp/activity/SignupActivity.java
+++ app/src/main/java/com/krystofmacek/firebasechatapp/activity/SignupActivity.java
@@ -1,16 +1,77 @@
 package com.krystofmacek.firebasechatapp.activity;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
 import com.krystofmacek.firebasechatapp.R;
 
+import java.util.Arrays;
+
 public class SignupActivity extends AppCompatActivity {
+
+    private AuthUI mAuthUI;
+    //request code value
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        mAuthUI = AuthUI.getInstance();
+
+
+        // Enable sign-in providers
+        startActivityForResult(mAuthUI
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(Arrays.asList(
+                                new AuthUI.IdpConfig.EmailBuilder().setRequireName(false).build(),
+                                new AuthUI.IdpConfig.GoogleBuilder().build()
+                        ))
+                        .setLogo(R.drawable.splash_image)
+                        .build(),
+                RC_SIGN_IN);
+
+        // TODO: přidat Facebook authenticaion
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            Intent intent;
+
+            // Successfully signed in
+            if (resultCode == RESULT_OK) {
+                intent = new Intent(SignupActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    Toast.makeText(this, "Sign in cancelled.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    Toast.makeText(this, "No internet connection.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Toast.makeText(this, "Unknown error", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 }
