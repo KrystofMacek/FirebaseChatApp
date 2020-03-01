c4706452203ff6fa23aa97ed188d2280b5908014
Krystof <krystofmacek333@gmail.com>
2020-03-01 17:36:16 +0100

    Added view pager for chats fragment - Tab for recent chats

---
+++ app/src/main/java/com/krystofmacek/firebasechatapp/adapters/ViewPagerAdapter.java
@@ -0,0 +1,41 @@
+package com.krystofmacek.firebasechatapp.adapters;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    private final List<String> mFragmentTitleList = new ArrayList<>();
+
+    public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
+    }
+
+}
--- app/src/main/java/com/krystofmacek/firebasechatapp/fragments/ChatsFragment.java
+++ app/src/main/java/com/krystofmacek/firebasechatapp/fragments/ChatsFragment.java
@@ -6,10 +6,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 
+import com.google.android.material.tabs.TabLayout;
 import com.krystofmacek.firebasechatapp.R;
+import com.krystofmacek.firebasechatapp.adapters.ViewPagerAdapter;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 public class ChatsFragment extends Fragment {
 
@@ -17,6 +22,26 @@ public class ChatsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chats, container, false);
+        View view = inflater.inflate(R.layout.fragment_chats, container, false);
+
+
+        ViewPager viewPager = view.findViewById(R.id.fChats_viewPager);
+        setupViewPager(viewPager);
+        TabLayout tabLayout = view.findViewById(R.id.fChats_tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+        return view;
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+
+        ViewPagerAdapter adapter =
+                new ViewPagerAdapter(getChildFragmentManager(),
+                        FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+
+        adapter.addFragment(new RecentConverasationsFragment(), "Recent");
+        viewPager.setAdapter(adapter);
+
     }
 }
