3df3706685b99422824bc820756688aae91f2f75
Krystof <krystofmacek333@gmail.com>
2020-03-01 17:00:36 +0100

    Added messaging activity and UI + message items

---
+++ app/src/main/java/com/krystofmacek/firebasechatapp/activity/MessagingActivity.java
@@ -0,0 +1,15 @@
+package com.krystofmacek.firebasechatapp.activity;
+
+
+import android.os.Bundle;
+import com.krystofmacek.firebasechatapp.R;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MessagingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messaging);
+    }
+}
